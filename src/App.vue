81e301c42853e8107d05dd08e22a3835f569556c
Emilia <emi.kamrowska@gmail.com>
2020-11-30 21:01:32 +0100

    Car component styles, removing car action, header styles

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <div id="app">
     <div class="header">
       <div class="header__title">Cars2go</div>
-      <button class="header__button">Add new car</button>
+      <div class="header__button">Add new car</div>
     </div>
     <div class="cars-container">
       <Car
         v-for="car in carList"
         :key="car.id"
-        :car="car"/>
+        :car="car"
+        @delete="deleteCar"/>
     </div>
   </div>
 </template>
@@ -27,15 +28,27 @@ export default {
       carList: CARS,
     }
   },
+  methods: {
+    deleteCar(id) {
+      this.carList = this.carList.filter((car) => car.id !== id)
+    }
+  }
 }
 </script>
 
 <style>
+:root {
+  --color-dark:#22313F;
+  --color-light-1: #F6F6F6;
+  --color-light-2: #A3A3A3;
+  --color-accent: #5DAC6E;
+}
 html,
 body {
   margin: 0;
   height: 100%;
   width: 100%;
+  font-family: "Roboto", sans-serif;
 }
 div {
   display:flex;
@@ -46,20 +59,40 @@ div {
 .flex-column{
   flex-direction:column;
 }
+.color-light-2{
+  color:var(--color-light-2);
+}
 
 #app {
   display:block;
   margin:auto;
   height:100%;
-  width:1200px;
+  width:1000px;
 }
 .header{
   height:100px;
   width:100%;
   justify-content: space-between;
-
+  background-color: var(--color-dark);
+  color: var(--color-light-1);
+}
+.header__title{
+  color:var(--color-light-1);
+  font-size:20px;
+  line-height:100px;
+  margin-left:20px;
+}
+.header__button{
+  line-height: 100px;
+  color: var(--color-light-1);
+  margin-right:20px;
+  cursor: pointer;
+  font-size:16px;
 }
 .cars-container{
   flex-flow: column nowrap;
+  background-color: var(--color-light-1);
+  padding:12px;
+  min-height:100%;
 }
 </style>
--- src/components/Car.vue
+++ src/components/Car.vue
@@ -6,19 +6,19 @@
         {{car.title}}
       </div>
       <div class="flex-row">
-        <div>{{car.persons}}</div>
-        <div>{{car.doors}}</div>
-        <div>{{car.fuel}}l/100km</div>
+        <div class="car__icon-chip"><img class="car__icon-chip__icon" src="@/assets/images/person_icon.png"/><span class="color-light-2">{{car.persons}}</span></div>
+        <div class="car__icon-chip"><img class="car__icon-chip__icon" src="@/assets/images/door_icon.png"/>{{car.doors}}</div>
+        <div class="car__icon-chip"><img class="car__icon-chip__icon" src="@/assets/images/fuel_icon.png"/>{{car.fuel}}l/100km</div>
       </div>
-      <div class="flex-row">
-        <div class="flex-column">
-          <div>Description</div>
-          <div>{{car.description}}</div>
+      <div>
+        <div class="car__description">
+          <div class="car__description__title">Description</div>
+          <div class="color-light-2">{{car.description}}</div>
         </div>
-        <div class="flex-column">
+        <div class="car__actions">
           <div class="car__price">${{car.price}}</div>
-          <button>Book Now</button>
-          <button>Delete car</button>
+          <div class="car__dark-button">Book Now</div>
+          <div class="car__delete-button" @click.prevent="$emit('delete', car.id)">Delete car</div>
         </div>
       </div>
     </div>
@@ -41,15 +41,70 @@ export default {
   flex-flow:row nowrap;
   height:400px;
   width:100%;
-  margin:10px;
+  margin-bottom:12px;
   background-color:white;
 }
 .car__img{
   display:flex;
+  width:43%;
   object-fit: contain;
 }
 .car__details{
-
+  flex-flow:column;
+  width:56%;
 }
+.car__title{
+  font-size: 30px;
+  font-weight:bold;
+  color: var(--color-dark);
+  margin:30px 0 20px 0;
+}
+.car__icon-chip{
+  border:solid 1px var(--color-light-2);
+  border-radius: 10px;
+  padding:5px;
+  margin-right:10px;
+}
+.car__icon-chip__icon{
+  height:20px;
+  width:20px;
+}
+.car__description{
+  flex-flow: column nowrap;
+  width:60%;
+  border-right: solid 1px var(--color-light-2);
+  padding-right:20px;
+}
+.car__description__title{
+  color: var(--color-dark);
+  margin: 20px 0;
+}
+.car__actions{
+  margin-left:20px;
+  flex-flow:column nowrap;
+}
+.car__price{
+  color:var(--color-accent);
+  font-size:40px;
+  font-weight:600;
+  margin-bottom: 20px;
+}
+.car__dark-button{
+  background-color: var(--color-dark);
+  color: var(--color-light-1);
+  padding:10px;
+  width:150px;
+  justify-content: center;
+  font-size:20px;
+}
+.car__delete-button{
+  color: var(--color-light-2);
+  width:150px;
+  justify-content: center;
+  padding:10px;
+  font-size:14px;
+  cursor: pointer;
+}
+
 </style>
 
